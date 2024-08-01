ee16dfdf24056961f1299e9356a2042172d30121
kudakov <kudakov@inspector-cloud.com>
2024-08-01 15:54:15 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -17,24 +17,24 @@ int main(int argc, char **argv) {
   qt.setRoot({0, 0}, {1000, 1000});
   qt.insert(&polygon);
 
-    cv::Point2i testPoint(0, 0);
-    cv::Point2i testPoint_1(25, 25);
+  cv::Point2i testPoint(0, 0);
+  cv::Point2i testPoint_1(25, 25);
   double result = cv::pointPolygonTest(polygon, testPoint, false);
-    std::cout << "test resul ->" << result << std::endl;
-//  double result_1 = cv::pointPolygonTest(polygon, testPoint_1, false);
+  std::cout << "test resul ->" << result << std::endl;
+  //  double result_1 = cv::pointPolygonTest(polygon, testPoint_1, false);
 
-    std::vector<cv::Point2i> * polygon_tst =  qt.getPointerPolygon(testPoint);
-    if(polygon_tst != nullptr) {
-        std::cout << "GOOD" << std::endl;
-    } else {
-        std::cout << "BAD" << std::endl;
-    }
-    std::vector<cv::Point2i> *  polygon_tst_1 =   qt.getPointerPolygon(testPoint_1);
-    if(polygon_tst_1 != nullptr) {
-        std::cout << "BAD" << std::endl;
-    } else {
-        std::cout << "GOOD" << std::endl;
-    }
+  std::vector<cv::Point2i> *polygon_tst = qt.getPointerPolygon(testPoint);
+  if (polygon_tst != nullptr) {
+    std::cout << "GOOD" << std::endl;
+  } else {
+    std::cout << "BAD" << std::endl;
+  }
+  std::vector<cv::Point2i> *polygon_tst_1 = qt.getPointerPolygon(testPoint_1);
+  if (polygon_tst_1 != nullptr) {
+    std::cout << "BAD" << std::endl;
+  } else {
+    std::cout << "GOOD" << std::endl;
+  }
 
   std::cout << "hello open cv " << std::endl;
   return 0;

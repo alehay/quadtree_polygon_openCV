3432c9b25fac24dfe27bda718cdc06a036cf7a88
kudakov <kudakov@inspector-cloud.com>
2024-10-31 13:28:47 +0300

    fix speed test measure,

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 it's just a quadtree created to store 4 coal polygons representing cv::Point . 
 and a quick check to see if the point falls into the polygon.
+
+
+
+speed test
+```
+QuadTree insertion time: 0.784 ms
+RTree insertion time: 1.036 ms
+QuadTree query time: 147.2 ms
+RTree query time: 10.438 ms
+Brute Force query time: 1620.97 ms
+Mismatches qt: 0
+```
+![Speed Test Results](field_visualization.png)
--- speed_test.cpp
+++ speed_test.cpp
@@ -21,6 +21,67 @@ std::vector<cv::Point2i> generateRandomPolygon(int minX, int maxX, int minY,
   return polygon;
 }
 
+cv::Scalar getColorFromNumbers(int number1, int number2)
+
+{
+  std::size_t h1 = std::hash<int>{}(number1);
+  std::size_t h2 = std::hash<int>{}(number2);
+  std::size_t res = h1 ^ (h2 << 1);
+
+  int r = (h1 % 128) + 128;
+  int g = (h2 % 128) + 128;
+  int b = (res % 128) + 128;
+
+  return cv::Scalar(b, g, r);
+}
+
+bool polygonsIntersect(const std::vector<cv::Point2i>& poly1, const std::vector<cv::Point2i>& poly2) {
+    std::vector<cv::Point2f> poly1f(poly1.begin(), poly1.end());
+    std::vector<cv::Point2f> poly2f(poly2.begin(), poly2.end());
+    cv::Mat intersectingRegion;
+    return cv::rotatedRectangleIntersection(cv::minAreaRect(poly1f), cv::minAreaRect(poly2f), intersectingRegion) != cv::INTERSECT_NONE;
+}
+
+
+std::vector<cv::Point2i> generateSmallRandomRectangle(int minX, int maxX, int minY, int maxY, int maxSize, const std::vector<std::vector<cv::Point2i>>& existingPolygons) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> disX(minX, maxX - maxSize);
+    std::uniform_int_distribution<> disY(minY, maxY - maxSize);
+    std::uniform_int_distribution<> disSize(10, maxSize);
+
+    for (int attempts = 0; attempts < 100; ++attempts) {
+        int topLeftX = disX(gen);
+        int topLeftY = disY(gen);
+        int width = disSize(gen);
+        int height = disSize(gen);
+
+        std::vector<cv::Point2i> rectangle = {
+            {topLeftX, topLeftY},
+            {topLeftX + width, topLeftY},
+            {topLeftX + width, topLeftY + height},
+            {topLeftX, topLeftY + height}
+        };
+
+        bool intersects = false;
+        for (const auto& existingPolygon : existingPolygons) {
+            if (polygonsIntersect(rectangle, existingPolygon)) {
+                intersects = true;
+                break;
+            }
+        }
+
+        if (!intersects) {
+            return rectangle;
+        }
+    }
+
+    // If we couldn't generate a non-intersecting polygon after 100 attempts, return an empty vector
+    return {};
+}
+
+
+
 // Function to measure execution time
 template <typename Func> double measureExecutionTime(Func func) {
   auto start = std::chrono::high_resolution_clock::now();
@@ -50,14 +111,17 @@ bruteForceQuery(const cv::Point2i &point,
 }
 
 int main() {
-  const int NUM_POLYGONS = 1 << 16;
-  const int NUM_QUERIES = 1 << 16;
-  const int WORLD_SIZE = 1 << 12;
+  const int NUM_POLYGONS = 1 << 12;
+  const int NUM_QUERIES = 1 << 15;
+  const int WORLD_SIZE = 1 << 11;
 
-  std::vector<std::vector<cv::Point2i>> polygons;
-  for (int i = 0; i < NUM_POLYGONS; ++i) {
-    polygons.push_back(generateRandomPolygon(0, WORLD_SIZE, 0, WORLD_SIZE, 4));
-  }
+    std::vector<std::vector<cv::Point2i>> polygons;
+    for (int i = 0; i < NUM_POLYGONS; ++i) {
+        auto newPolygon = generateSmallRandomRectangle(0, WORLD_SIZE, 0, WORLD_SIZE, 100, polygons);
+        if (!newPolygon.empty()) {
+            polygons.push_back(newPolygon);
+        }
+    }
 
   QuadTree<int, std::vector<cv::Point2i> *> qt;
   RTree<int, std::vector<cv::Point2i> *> rt;
@@ -142,6 +206,35 @@ int main() {
             << "Good matches: " << good_match_qt << "\n";
   std::cout << "Mismatches rt: " << miss_match_rt << "\n"
             << "Good matches: " << good_match_rt << "\n";
+
+      // Create an image to visualize the field
+    cv::Mat image(WORLD_SIZE, WORLD_SIZE, CV_8UC3, cv::Scalar(255, 255, 255));
+
+    // Draw polygons
+    for (const auto& polygon : polygons) {
+        std::vector<std::vector<cv::Point>> contours = {polygon};
+        cv::drawContours(image, contours, 0, getColorFromNumbers(contours[0][0].x, contours[0][0].y ), 2);
+    }
+
+    // Draw query points
+    for (const auto& point : queryPoints) {
+        cv::circle(image, point, 2, cv::Scalar(0, 0, 255), -1);
+    }
+
+    // Resize the image for better visibility (optional)
+    cv::Mat resized_image;
+    cv::resize(image, resized_image, cv::Size(), 0.5, 0.5);
+
+    // Display the image
+    cv::imshow("Field with Polygons and Query Points", resized_image);
+    cv::waitKey(0);
+
+    // Save the image
+    cv::imwrite("field_visualization.png", resized_image);
+
+    // ... (keep your existing code for performance measurements and result comparison)
+
+    return 0;
 
   return 0;
 }
